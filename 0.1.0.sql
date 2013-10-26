3493b3103b413f0ce6d8c0b8fd7b6e32b519f1ab
Patrick Reynolds <cpreynolds@gmail.com>
2013-10-26 00:56:52 -0400

    Adding a start on a db schema.

---
+++ 0.1.0.sql
@@ -0,0 +1,15 @@
+CREATE TABLE pomodoro (
+    user_id bigserial primary key,
+    email varchar(255) NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    token varchar(255) NOT NULL,
+);
+
+CREATE TABLE pomodoro (
+    pomodoro_id bigserial primary key,
+    user_id bigserial,
+    description varchar(255) NOT NULL,
+    time_started timestamp,
+    time_ended timestamp,
+);
